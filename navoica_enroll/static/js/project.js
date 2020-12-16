93a4fdfe51874849e55e8b270b92231551c29b8f
Paula Wasylów <67696025+paula-opi@users.noreply.github.com>
2020-12-16 11:09:20 +0100

    MOOC-1599 Style: add new styling for register form on course (#3)
    Added disable inputs on choosen other (pesel/no pesel, country/vovidoship/county, job status/profession)
    Added translations
    Added checkbox no pesel
    Added no filled age input
    Added new style for register form course
    Added unit tests javascript project.js with Jest

--- navoica_enroll/static/js/project.js
+++ navoica_enroll/static/js/project.js
@@ -1,1 +1,131 @@
 /* Project specific Javascript goes here. */
+
+function loadRegisterForm() {
+  let countryInput = document.getElementById("id_country");
+  let countyInput = document.getElementById("id_county");
+  let voivodeshipInput = document.getElementById("id_voivodeship");
+  let communeInput = document.getElementById("id_commune");
+  let statusJobInput = document.getElementById("id_status");
+  let jobProfessionInput = document.getElementById("id_profession");
+  let workNameInput = document.getElementById("id_work_name");
+  let peselInput = document.getElementById("id_pesel");
+  let noPeselInput = document.getElementById("id_no_pesel");
+  let submitBtn = document.getElementById("submit-id-submit");
+  let hintWorkName = document.querySelector("#hint_id_work_name");
+
+  let parentNoPesel = noPeselInput.parentNode;
+  parentNoPesel.classList.add("align-self-center");
+  let parentParentNopesel = parentNoPesel.parentNode;
+  parentParentNopesel.classList.add("d-flex");
+  parentParentNopesel.classList.add("h-100");
+  parentParentNopesel.classList.add("mt-2");
+  hintWorkName.classList.add("mb-5");
+  hintWorkName.classList.add("mb-md-0");
+
+  function enableInputsLocation() {
+    countyInput.removeAttribute("disabled");
+    communeInput.removeAttribute("disabled");
+    voivodeshipInput.removeAttribute("disabled");
+    document.querySelector("[for='id_county']").classList.remove("disabled");
+    document
+      .querySelector("[for='id_voivodeship']")
+      .classList.remove("disabled");
+    document.querySelector("[for='id_commune']").classList.remove("disabled");
+  }
+
+  function disableInputsLocation() {
+    countyInput.setAttribute("disabled", "true");
+    communeInput.setAttribute("disabled", "true");
+    voivodeshipInput.setAttribute("disabled", "true");
+    document.querySelector("[for='id_county']").classList.add("disabled");
+    document.querySelector("[for='id_voivodeship']").classList.add("disabled");
+    document.querySelector("[for='id_commune']").classList.add("disabled");
+  }
+
+  function changeVisibilityInputsLocation(value) {
+    if (value.toLowerCase() === "polska" || value.toLowerCase() === "poland") {
+      enableInputsLocation();
+    } else {
+      disableInputsLocation();
+    }
+  }
+
+  function disableInputsStatusJob() {
+    jobProfessionInput.setAttribute("disabled", "true");
+    workNameInput.setAttribute("disabled", "true");
+    document.querySelector("[for='id_profession']").classList.add("disabled");
+    document.querySelector("[for='id_work_name']").classList.add("disabled");
+  }
+
+  function enableInputsStatusJob() {
+    jobProfessionInput.removeAttribute("disabled");
+    workNameInput.removeAttribute("disabled");
+    document
+      .querySelector("[for='id_profession']")
+      .classList.remove("disabled");
+    document.querySelector("[for='id_work_name']").classList.remove("disabled");
+  }
+
+  function changeVisibilityJob(value) {
+    const hasJob = statusJobInput[1].value;
+    if (value.toLowerCase() === hasJob.toLowerCase()) {
+      enableInputsStatusJob();
+    } else {
+      disableInputsStatusJob();
+    }
+  }
+
+  function checkPeselInput() {
+    if (peselInput.value === "" && !noPeselInput.checked) {
+      peselInput.setAttribute("required", "true");
+    } else {
+      peselInput.removeAttribute("required");
+    }
+
+    if (peselInput.value !== "") {
+      noPeselInput.setAttribute("disabled", "true");
+      document.querySelector("[for='id_no_pesel']").classList.add("disabled");
+    } else {
+      noPeselInput.removeAttribute("disabled");
+      document
+        .querySelector("[for='id_no_pesel']")
+        .classList.remove("disabled");
+    }
+
+    if (noPeselInput.checked) {
+      peselInput.setAttribute("disabled", "true");
+      peselInput.removeAttribute("required");
+      document.querySelector("[for='id_pesel']").classList.add("disabled");
+    } else {
+      peselInput.removeAttribute("disabled");
+      peselInput.setAttribute("required", "true");
+      document.querySelector("[for='id_pesel']").classList.remove("disabled");
+    }
+  }
+
+  checkPeselInput();
+  changeVisibilityInputsLocation(countryInput.value);
+  changeVisibilityJob(statusJobInput.value);
+
+  countryInput.addEventListener("input", function (e) {
+    changeVisibilityInputsLocation(e.target.value);
+  });
+
+  statusJobInput.addEventListener("change", function () {
+    changeVisibilityJob(this.value);
+  });
+
+  peselInput.addEventListener("input", function () {
+    checkPeselInput();
+  });
+
+  noPeselInput.addEventListener("change", function () {
+    checkPeselInput();
+  });
+
+  submitBtn.addEventListener("click", function () {
+    checkPeselInput();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", loadRegisterForm);
--- navoica_enroll/users/models.py
+++ navoica_enroll/users/models.py
@@ -8,7 +8,6 @@ from django.utils.translation import ugettext_lazy as _
 from localflavor.pl.pl_administrativeunits import ADMINISTRATIVE_UNIT_CHOICES
 from localflavor.pl.pl_voivodeships import VOIVODESHIP_CHOICES
 
-
 class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, null=True, max_length=255)
 
@@ -22,15 +21,17 @@ class UserRegistrationCourse(models.Model):
     first_name = models.CharField(_("First Name"), max_length=100)
     last_name = models.CharField(_("Last name"), max_length=100)
     gender = models.CharField(_("Gender"), choices=[
+        ('', _('Choose gender...')),
         ('M', _('Male')),
         ('F', _('Female')),
     ], max_length=1)
     pesel = models.CharField(_("PESEL"), null=True, blank=True, max_length=11,
                              help_text=_(
                                  "For people who do not have a PESEL number, type in NONE."))
-    age = models.SmallIntegerField(_("Age"), default=18)
+    age = models.SmallIntegerField(_("Age"), default="")
     education = models.CharField(_("Education"), max_length=1,
                                  choices=[
+                                     ('', _('Choose grade...')),
                                      ('1', _('Pre-primary')),
                                      ('2', _('Primary')),
                                      ('3', _('Secondary')),
@@ -46,11 +47,11 @@ class UserRegistrationCourse(models.Model):
                                           help_text=_("May contain letters"))
     postal_code = models.CharField(_("Postal code"), max_length=6)
     city = models.CharField(_("City"), max_length=30)
-    voivodeship = models.CharField(_("Voivodeship"), max_length=30, null=True,
+    voivodeship = models.CharField(_("Voivodeship"), default="", max_length=30, null=True,
                                    blank=True,
                                    choices=sorted(VOIVODESHIP_CHOICES,
                                                   key=lambda x: x[1]))
-    county = models.CharField(_("County"), max_length=30, null=True, blank=True,
+    county = models.CharField(_("County"), default="", max_length=30, null=True, blank=True,
                               choices=sorted(ADMINISTRATIVE_UNIT_CHOICES_FIXED,
                                              key=lambda x: x[1]))
     commune = models.CharField(_("Commune"), max_length=30, null=True,
@@ -71,17 +72,19 @@ class UserRegistrationCourse(models.Model):
                                           default=timezone.now)
 
     STATUSES = [
+        _("Choose employed status..."),
         _("Employed"),
         _("Registered unemployed"),
         _("Unregistered unemployed"),
         _("Unemployed, not looking for work")
     ]
 
-    status = models.CharField(_("Status"), max_length=1000,
+    status = models.CharField(_("Status"), default="", max_length=1000,
                               choices=[(t, t) for t in STATUSES]
                               )
 
     PROFESSIONS = [
+        _("Choose profession..."),
         _("Vocational teacher"),
         _("General education teacher"),
         _("Kindergarten teacher"),
@@ -97,27 +100,28 @@ class UserRegistrationCourse(models.Model):
         _("Other"),
     ]
 
-    profession = models.CharField(_("Profession"), max_length=1000,
+    profession = models.CharField(_("Profession"), default="test", max_length=1000,
                                   choices=[(t, t) for t in
                                            PROFESSIONS])
 
     work_name = models.CharField(_("Job title"), max_length=1000, help_text=_(
         "Abbreviations not allowed, full name of the institution"))
-    origin = models.CharField(_("Migrant / ethnic minority"), max_length=1,
-                              choices=[
-                                  ('y', _("Yes")),
-                                  ('n', _("No")),
-                                  ('r', _("Prefer not to tell"))
 
-                              ]
-                              )
-    homeless = models.CharField(_("Homeless"), max_length=1, choices=[
+
+
+    origin = models.CharField(_("Migrant / ethnic minority"), max_length=1, default="", choices=[
         ('y', _("Yes")),
         ('n', _("No")),
         ('r', _("Prefer not to tell"))
 
     ])
-    disabled_person = models.CharField(_("Disabled person"), max_length=1,
+    homeless = models.CharField(_("Homeless"), max_length=1, default="", choices=[
+        ('y', _("Yes")),
+        ('n', _("No")),
+        ('r', _("Prefer not to tell"))
+
+    ])
+    disabled_person = models.CharField(_("Disabled person"), max_length=1, default="",
                                        choices=[
                                            ('y', _("Yes")),
                                            ('n', _("No")),
@@ -125,11 +129,10 @@ class UserRegistrationCourse(models.Model):
 
                                        ])
     social_disadvantage = models.CharField(_("Socially disadvantaged"),
-                                           max_length=1, choices=[
+                                           max_length=1,  default="",choices=[
             ('y', _("Yes")),
             ('n', _("No")),
             ('r', _("Prefer not to tell"))
-
         ])
 
     course_id = models.CharField(max_length=1000)
